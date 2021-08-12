bdfc1248532aad8544bee71b23fd58f21de15852
anjaliaryain <68144519+anjaliaryain@users.noreply.github.com>
2021-08-12 19:43:17 +0530

    Task 9 SIG WEB
    Task 9 Completed

---
+++ Task9/index.mjs
@@ -0,0 +1,9 @@
+import fs, { writeFileSync } from "fs";
+// fs.mkdirSync("Sig Web Task");
+// fs.writeFileSync("Sig Web Task/read.txt", "This is Sig Web Task 9.");
+// fs.appendFileSync("Sig Web Task/read.txt", "It is based on the concept of node.js.");
+// const data = fs.readFileSync("Sig Web Task/read.txt", "utf-8");
+// console.log(data);
+// fs.renameSync("Sig Web Task/read.txt", "Sig Web Task/readNew.txt");
+// fs.unlinkSync("Sig Web Task/readNew.txt");
+fs.rmdirSync("Sig Web Task");
